8471ecce22ec654decfa82b743d39ca09c038fb5
AnhTuanHa <aha@miu.edu>
2024-05-31 15:02:12 -0500

    Tuan commit

---
+++ src/lesson3/Prob4/PizzaSize.java
@@ -0,0 +1,7 @@
+package lesson3.Prob4;
+
+public enum PizzaSize {
+    SMALL,
+    MEDIUM,
+    LARGE
+}
---
+++ src/lesson3/Prob4/PizzaType.java
@@ -0,0 +1,8 @@
+package lesson3.Prob4;
+
+public enum PizzaType {
+    VEGGIE,
+    PEPPERONI,
+    CHEEZE,
+    BBQ_CHICKEN,
+}
