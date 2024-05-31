93cd78ea0a507d8a64795574d9e68479c03f3da9
AnhTuanHa <tuanha17897@gmail.com>
2024-05-31 15:22:06 -0500

    Revert "Tuan commit"
    This reverts commit 8471ecce22ec654decfa82b743d39ca09c038fb5.

--- src/lesson3/Prob4/PizzaSize.java
+++
@@ -1,7 +0,0 @@
-package lesson3.Prob4;
-
-public enum PizzaSize {
-    SMALL,
-    MEDIUM,
-    LARGE
-}
--- src/lesson3/Prob4/PizzaType.java
+++
@@ -1,8 +0,0 @@
-package lesson3.Prob4;
-
-public enum PizzaType {
-    VEGGIE,
-    PEPPERONI,
-    CHEEZE,
-    BBQ_CHICKEN,
-}
