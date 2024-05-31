d41f195154470cda62c155674434c48b5eabd391
ThezThaw <97180604+ThezThaw@users.noreply.github.com>
2024-05-31 17:00:28 -0500

    Added problem 5

---
+++ src/lesson3/Prob5/Circle.java
@@ -0,0 +1,21 @@
+package lesson3.Prob5;
+
+public final class Circle {
+
+    private final double radius;
+
+    public Circle(double radius)
+    {
+        this.radius=radius;
+    }
+
+    public double computeArea()
+    {
+        return Math.PI * this.radius * this.radius;
+    }
+
+    public double getRadius()
+    {
+        return this.radius;
+    }
+}
